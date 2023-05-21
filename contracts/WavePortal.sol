692057351574187251cfc1a2cf76b064e92db83f
rwspatin <rwspatin@gmail.com>
2023-05-21 13:05:00 -0300

    Create waives structure to set msg on interactions

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,18 +8,48 @@ contract WavePortal {
     //This state variable is saved permanently on the contract
     uint256 totalWaves;
 
+    /*
+     * Um pouco de mágica, use o Google para entender o que são eventos em Solidity!
+     */
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    /*
+     * Crio um struct Wave.
+     * Um struct é basicamente um tipo de dados customizado onde nós podemos customizar o que queremos armazenar dentro dele
+     */
+    struct Wave {
+        address waver; // Endereço do usuário que deu tchauzinho
+        string message; // Mensagem que o usuário envio
+        uint256 timestamp; // Data/hora de quando o usuário tchauzinhou.
+    }
+
+    /*
+     * Declara a variável waves que permite armazenar um array de structs.
+     * Isto que me permite armazenar todos os tchauzinhos que qualquer um tenha me enviado!
+     */
+    Wave[] waves;
+
     constructor() {
         console.log("SmartContract Started. Let's the game begin");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
         //msg.sender is the user's wallet address that called the function wave
-        console.log("%s May The Force Be With You!", msg.sender);
+        console.log("%s May The Force Be With You! %s", msg.sender, _message);
+
+        //push the mensage on array
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
     }
 
     function getTotalWaves() public view returns (uint256) {
         console.log("We have %d 'people been one with the force'!", totalWaves);
         return totalWaves;
+    }
+
+    function getAllWaves() public view returns (Wave[] memory){
+        return waves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,5 @@
 //Deploy.js create a local blockchain and destroy it when finish runnig
 const main = async () => {
-    //Get the owner's wallet address and get a random address wallet
-    const [owner, randomPerson] = await hre.ethers.getSigners();
-
     //Locate the Smart contract
     //O Hardhat Runtime Environment, ou HRE abreviado, é um objeto que contém todas as funcionalidades que a Hardhat expõe ao executar uma tarefa, teste ou script. Na realidade, Hardhat é o HRE.
     //HRE doc link -> https://hardhat.org/hardhat-runner/docs/advanced/hardhat-runtime-environment
@@ -15,21 +12,27 @@ const main = async () => {
 
     //Show where this blockchain is located
     console.log("Contract deployed to:", waveContract.address);
-    //Show the contract deployer's address
-    console.log("Contract deployed by:", owner.address);
-
+    
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
+    console.log("Interactions count:", waveCount.toNumber())
 
-    let waveTxn = await waveContract.wave();
-    await waveTxn.wait();
+    /*
+    Sending a interactions with message
+    */
+    let waveTxn = await waveContract.wave("I'm one with the force and the force is with me");
+    await waveTxn.wait(); //storing the message to be minered
 
-    waveCount = await waveContract.getTotalWaves();
+    //Get the owner's wallet address and get a random address wallet
+    const [_, randomPerson] = await hre.ethers.getSigners();
 
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    await waveTxn.wait();
+    waveTxn = await waveContract.connect(randomPerson).wave("Maibe a sith here");
+    await waveTxn.wait(); //storing another message to be minered
 
-    waveCount = await waveContract.getTotalWaves();
+    let allWaives = await waveContract.getTotalWaves();
+    let waivesList = await waveContract.getAllWaves();
+    console.log("All interactions: ", allWaives);
+    console.log("List of interactions: ", waivesList)
   };
   
   const runMain = async () => {
